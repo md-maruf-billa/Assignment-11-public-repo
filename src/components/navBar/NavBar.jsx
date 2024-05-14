972564fd123697c166cc9a7ebbc3a56a4aa4921b
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2024-05-14 23:37:30 +0600

    Fixed search box design

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -10,14 +10,16 @@ import { updateProfile } from 'firebase/auth';
 import auth from '../../utils/firebase/firebase.config';
 import { Tooltip } from 'react-tooltip';
 import axios from 'axios';
+import notFound from '../../assets/animation/notFound.json'
+import Lottie from 'lottie-react';
 
 const NavBar = () => {
 
     //------------get user from context----------
     const { currentUser, logOutUser, setLoading } = useContext(userDataContext);
     const [searchData, setSearchData] = useState([]);
-    const [focus, setFocus] = useState(false);
-    const [theme,setTheme] = useState(localStorage.getItem('theme')? localStorage.getItem('theme'):'light')
+    const [inputValue, setInputValue] = useState("");
+    const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
     //-------------Log out user---------
     const handelLogOut = () => {
         logOutUser()
@@ -80,16 +82,26 @@ const NavBar = () => {
 
     //--------------handel search area-----------
     const handelSearch = (e) => {
-        if (e.target.value == "") {
-            setSearchData([]);
-            setFocus(false)
-            return;
+        if (e.target.value !== "") {
+            setInputValue(e.target.value);
         }
-        axios.get(import.meta.env.VITE_API_URL+`/search/${e.target.value}`)
+        else{
+            setInputValue("")
+        }
+
+    }
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_API_URL + `/search/${inputValue}`)
             .then(res => {
-                setSearchData(res?.data)
-                setFocus(true);
+                setSearchData(res.data)
             })
+            .catch(err => {
+                setSearchData([])
+            })
+    },[inputValue])
+    const closeWindow = () => {
+        setInputValue('')
+        setSearchData([]);
     }
 
 
@@ -138,27 +150,32 @@ const NavBar = () => {
                 <div className="navbar-end gap-5">
                     <div className='relative hidden md:block'>
                         <label className="input input-bordered bg-transparent flex items-center gap-2">
-                            <input onChange={handelSearch} type="text" className="grow " placeholder="Search" />
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                            <input onChange={handelSearch} type="text" className="grow " value={inputValue} placeholder="Search" />
+                            {inputValue == 0 ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg> :
+                                <RxCross2 onClick={closeWindow} className='z-50 text-xl cursor-pointer' />}
                         </label>
-                        <div className={`${focus ? "" : 'hidden'} min-h-[300px] w-[600px] absolute bg-[linear-gradient(180deg,rgba(0,0,0,0.6),rgba(0,0,0,0.6))] top-20 right-0 z-50 text-white p-5 rounded-md`}>
+                        <div
+                            className={`${inputValue ? "" : 'hidden'} min-h-[300px] max-h-[50vh] overflow-y-auto w-[600px] absolute bg-green-200 top-20 right-0 z-50 p-5 rounded-md`}>
                             {
-                                searchData.length == 0 ? <p>No Mach Search</p> : <div className='relative'>
+                                searchData.length === 0 ? <div>
 
-
-                                    <RxCross2 onClick={() => setFocus(false)} className='absolute -top-8 right-0 text-xl cursor-pointer' />
+                                    <Lottie className='h-[300px]' animationData={notFound}></Lottie>
+                                    <p className='text-center'>No Data Found</p>
+                                </div> : <div className='relative'>
 
                                     {
                                         searchData?.map(data => <div key={data._id} className='mt-5' >
-                                            <div className='flex items-center gap-5 mt-3 border border-green-400 p-4 rounded-md'>
-                                                <img className='size-[60px] rounded-full' src={data?.photoURL} alt="" />
-                                                <div className='w-1/2'>
-                                                    <h2 className='text-2xl'>{data?.serviceName}</h2>
-                                                    <p className='flex items-center gap-2'><FaLocationDot className='text-red-500' /> {data?.serviceArea}</p>
+                                            <div className='grid grid-cols-8 items-center gap-5 mt-3 border border-green-400 p-2 rounded-md'>
+                                                <img className='size-[30px] rounded-full' src={data?.photoURL} alt="" />
+                                                <div className='col-span-6'>
+                                                    <h2 className=''>{data?.serviceName}</h2>
+                                                    <p className='flex items-center gap-2 text-xs'><FaLocationDot className='text-red-500' /> {data?.serviceArea}</p>
                                                 </div>
-                                                <div className='flex justify-end w-full'>
-                                                    <Link to={`/service-details/${data._id}`}>
-                                                        <Button btnName={"View Details"} />
+                                                <div className='flex justify-end'>
+                                                    <Link
+                                                    onClick={closeWindow}
+                                                    className='btn btn-xs btn-success text-white' to={`/service-details/${data._id}`}>
+                                                        View More
                                                     </Link>
                                                 </div>
                                             </div>
@@ -171,11 +188,11 @@ const NavBar = () => {
                     <label className="swap swap-rotate">
 
                         {/* this hidden checkbox controls the state */}
-                        <input 
-                         onChange={handelThem} 
-                         type="checkbox" 
-                         checked={theme=='light'?false: true}
-                         className="theme-controller" />
+                        <input
+                            onChange={handelThem}
+                            type="checkbox"
+                            checked={theme == 'light' ? false : true}
+                            className="theme-controller" />
 
                         {/* sun icon */}
                         <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
